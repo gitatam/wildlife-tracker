25ab1336589b3a158e3af8b4261d5e60dc4d5825
Mathew Gitata <gitataIT@gmail.com>
2019-11-25 21:10:54 +0300

    Add a dao layer for animal objects

---
+++ src/main/java/dao/AnimalDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import exc.DaoException;
+import model.Animal;
+
+import java.util.List;
+
+public interface AnimalDao {
+    void add(Animal animal) throws DaoException;
+
+    List<Animal> getAll();
+}
