d800242aa9514b4565ab5b86814e1681eb83b18f
Mathew Gitata <gitataIT@gmail.com>
2019-11-25 22:18:55 +0300

    Add add animals method implementation

--- src/main/java/dao/Sql2oAnimalDao.java
+++ src/main/java/dao/Sql2oAnimalDao.java
@@ -1,4 +1,37 @@
 package dao;
 
-public class Sql2oAnimalDao {
+import exc.DaoException;
+import model.Animal;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oAnimalDao implements AnimalDao {
+
+    private final Sql2o sql2o;
+
+    public Sql2oAnimalDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Animal animal) throws DaoException {
+        String sql = "INSERT INTO animals(name) VALUES (:name)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql)
+                    .bind(animal)
+                    .executeUpdate()
+                    .getKey();
+            animal.setId(id);
+        } catch (Sql2oException ex) {
+            throw new DaoException(ex, "Problem adding animal.");
+        }
+    }
+
+    @Override
+    public List<Animal> getAll() {
+        return null;
+    }
 }
