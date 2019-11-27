49654da2eda07751c9696d54129bac2396c10870
Mathew Gitata <gitataIT@gmail.com>
2019-11-27 09:01:21 +0300

    Deploy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,22 +4,59 @@ import dao.Sql2oAnimalDao;
 import dao.Sql2oSightingDao;
 import exc.ApiError;
 import exc.DaoException;
-import model.Animal;
 import model.Sighting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.*;
 
 public class App {
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
+            try {
+                if (System.getenv("DATABASE_URL") == null) {
+                    dbUri = new URI("postgres://localhost:5432/to_do");
+                    sql2o = new Sql2o("jdbc:postgresql://" + "localhost:5432" + "wildlife_tracker", "gitata", "password");
+                } else {
+                    dbUri = new URI(System.getenv("DATABASE_URL"));
+                    int dbport = dbUri.getPort();
+                    String host = dbUri.getHost();
+                    String path = dbUri.getPath();
+                    String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
+                    String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
+                    sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + dbport + path, username, password);
+                }
+
+            } catch (URISyntaxException e) {
+                logger.error("Unable to connect to database.");
+            }
+
         staticFileLocation("/public");
-        String connectionString = "jdbc:postgresql://localhost:5432/wildlife_tracker";
-        Sql2o sql2o = new Sql2o(connectionString, "gitata", "password");
         AnimalDao animalDao = new Sql2oAnimalDao(sql2o);
         SightingDao sightingDao = new Sql2oSightingDao(sql2o);
 
